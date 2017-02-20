a259583cc5de55cbe59224cb21d1dcc45d1e295b
Maxwell O'Roark <m.d.oroark@gmail.com>
2017-02-19 18:01:03 -0700

    2.1

--- ch2/server.js
+++ ch2/server.js
@@ -3,6 +3,8 @@ var querystring = require('querystring');
 var util = require('util')
 var form = require('fs').readFileSync('form.html');
 
+var maxData = 2 * 1024 * 1024; // 2mb
+
 http.createServer((request, response) => {
   if (request.method === 'GET') {
     response.writeHead(200, {'Content-Type': 'text/html'})
@@ -10,9 +12,20 @@ http.createServer((request, response) => {
   }
   if (request.method === 'POST') {
     var postData = ''
-    request.on('data', (chunk) => {
+    request.on('data', function(chunk) {
       postData += chunk;
+      if (postData.length > maxData) {
+        postData = '';
+        this.pause();
+        response.writeHead(413); // request entity too large
+        response.end('Too large')
+      }
     }).on('end', () => {
+      if (!postData) {
+        console.log('no data submitted...')
+        response.end();
+        return;
+      }
       var postDataObject = querystring.parse(postData);
       console.log('User Posted: \n ' + postData);
       response.end('You posted: \n' + util.inspect(postDataObject));
